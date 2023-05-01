c35a9f84206f4fa2f256e5197e1a334e3d4c5d4b
unknown <musfiquzzama1999@gmail.com>
2023-05-02 00:28:55 +0600

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT||5000;
+
+app.use(cors())
+
+const chefs = require('./data/chef.json');
 
 app.get('/', (req, res) => {
   res.send('chef recipe hunder site!')
 })
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
